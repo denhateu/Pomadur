87cd3b8b719f92887a75b187dbd789a5690605eb
denhateu <danilaksenow101@gmail.com>
2025-01-23 21:45:58 +0500

    feat: added new modes for pomodoro

--- scripts/main.py
+++ scripts/main.py
@@ -5,12 +5,48 @@ import time
 class Pomadur:
   """It is my own class for Pomadur app"""
 
-  def __init__(self, work_minutes, break_minutes, big_break_time):
+  def __init__(self, work_minutes, break_minutes, big_break_time, pomodors_before_big_break):
     self.work_minutes = work_minutes
     self.break_minutes = break_minutes
     self.big_break_time = big_break_time
+    self.pomodors_before_big_break = pomodors_before_big_break
 
     self.pomadur_thread = None
+    self.pomadur_seeker_thread = None
+
+    self.pomadur_alive = False
+    self.pomadur_ended = False
+
+  def start_pomadur_seeker(self):
+    current_mode = "work"
+    self.pomodors_left_before_big_break = self.pomodors_before_big_break
+
+    self.pomadur_thread = threading.Thread(target=self.start_pomadur, args=(current_mode,))
+    self.pomadur_thread.start()
+
+    while self.pomadur_alive:
+      if self.pomadur_ended:
+        self.pomadur_thread.join()
+        
+        self.pomadur_ended = False
+
+        if current_mode == "work":
+          self.pomodors_left_before_big_break -= 1
+
+          if self.pomodors_left_before_big_break == 0:
+            current_mode = "bigbreak"
+          else:
+            current_mode = "break"
+        elif current_mode == "break":
+          current_mode = "work"
+        elif current_mode == "bigbreak":
+          self.pomodors_left_before_big_break = self.pomodors_before_big_break
+          current_mode = "work"
+
+        self.pomadur_thread = threading.Thread(target=self.start_pomadur, args=(current_mode,))
+        self.pomadur_thread.start()
+
+      time.sleep(1)
 
   def start_pomadur(self, mode="work"):
     # Gets pomadur mode and minutes for it's mode
@@ -22,6 +58,15 @@ class Pomadur:
       pomadoro_minutes = self.big_break_time
 
     pomadoro_seconds = pomadoro_minutes * 60
+
+    print()
+    print("================")
+    print(f"Mode: {mode}")
+    print(f"Minutes: {pomadoro_minutes}")
+    print(f"Total seconds: {pomadoro_seconds}")
+    print(f"Big break after: {self.pomodors_left_before_big_break}")
+    print("================")
+    print()
 
     seconds_counter = 0
 
@@ -49,22 +94,34 @@ class Pomadur:
 
       time.sleep(1)
 
+    self.pomadur_ended = True
+
+    print("=======")
+    print(" ended ")
+    print("=======")
+
   def start(self):
-    self.pomadur_thread = threading.Thread(target=self.start_pomadur, args=("work",))
-    self.pomadur_thread.start()
+    self.pomadur_alive = True
+
+    self.pomadur_seeker_thread = threading.Thread(target=self.start_pomadur_seeker, args=())
+    self.pomadur_seeker_thread.start()
 
   def stop(self):
+    self.pomadur_alive = False
+
     self.pomadur_thread.join()
+    self.pomadur_seeker_thread.join()
 
 
 def main():
   # Setup pomodoro minutes
   work_time = 1
-  break_time = 5
-  big_break_time = 15
+  break_time = 1
+  big_break_time = 1
+  pomodors_before_big_break = 2
 
   # Starts pomodoro counter
-  pomadur = Pomadur(work_time, break_time, big_break_time)
+  pomadur = Pomadur(work_time, break_time, big_break_time, pomodors_before_big_break)
   pomadur.start()
 
 if __name__ == "__main__":
