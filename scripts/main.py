6048217b1e7401965869a3be5cba07d63965fa6d
denhateu <danilaksenow101@gmail.com>
2025-01-23 16:54:36 +0500

    feat: added Pomadur class

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,47 @@
+import threading
+import time
+
+
+class Pomadur:
+  def __init__(self, work_minutes, break_minutes):
+    self.work_minutes = work_minutes
+    self.break_minutes = break_minutes
+
+    self.pomadur_thread = None
+    self.pomadur_alive = False
+
+  def start_pomadur(self):
+    counter = 0
+
+    while self.pomadur_alive:
+      if counter >= 25:
+        self.pomadur_alive = False
+
+      print(f"counter: {counter}")
+
+      counter += 1
+
+      time.sleep(1)
+
+  def start(self):
+    self.pomadur_alive = True
+
+    self.pomadur_thread = threading.Thread(target=self.start_pomadur, args=())
+    self.pomadur_thread.start()
+
+  def stop(self):
+    self.pomadur_alive = False
+    self.pomadur_thread.join()
+
+
 def main():
-  print("Hell")
+  #work_time = 25
+  work_time = 6
+  break_time = 5
+
+  pomadur = Pomadur(work_time, break_time)
+  pomadur.start()
+  #pomadur.stop()
 
 if __name__ == "__main__":
   main()
