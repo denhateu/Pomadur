5f36ed412b64bcc2c5d1109a3fd9633388486552
denhateu <danilaksenow101@gmail.com>
2025-01-24 20:29:23 +0500

    feat: added create config file if not exists and read config file

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,57 @@
 import time
+import os
 from pyfiglet import Figlet
+import configparser
 
-from config import AUTHOR, APP_NAME, APP_VERSION
+from config import AUTHOR, APP_NAME, APP_VERSION, CONFIG_FILE
 from pomadur import Pomadur
 
 
 # Configure pyfiglet
 figlet = Figlet()
 
+
+def create_config(config_file) -> bool:
+  config = configparser.ConfigParser()
+
+  # Create default config settings
+  config["main"] = {
+    "work_minutes": 25,
+    "break_minutes": 5,
+    "big_break_minutes": 15,
+    "pomodors_before_big_break": 4,
+  }
+
+  with open(config_file, 'w', encoding="utf-8") as configfile:
+    config.write(configfile)
+
+  return True
+
+def read_config(config_file) -> dict:
+  config = configparser.ConfigParser()
+
+  # Checks if config file not exists
+  if os.path.exists(config_file) is False:
+    # Config not exists create default config file
+    create_config(config_file)
+
+  # Read the config file
+  config.read(config_file)
+
+  # Gets config file values
+  work_minutes = config.getint("main", "work_minutes")
+  break_minutes = config.getint("main", "break_minutes")
+  big_break_minutes = config.getint("main", "big_break_minutes")
+  pomodors_before_big_break = config.getint("main", "pomodors_before_big_break")
+
+  config_values = {
+    "work_minutes": work_minutes,
+    "break_minutes": break_minutes,
+    "big_break_minutes": big_break_minutes,
+    "pomodors_before_big_break": pomodors_before_big_break
+  }
+
+  return config_values
 
 def main():
   # Show logo
@@ -18,11 +62,14 @@ def main():
   print()
   time.sleep(3)
 
+  # Read config file for setup program
+  config_values = read_config(CONFIG_FILE)
+
   # Setup pomodoro minutes
-  work_time = 1
-  break_time = 1
-  big_break_time = 1
-  pomodors_before_big_break = 2
+  work_time = config_values["work_minutes"]
+  break_time = config_values["break_minutes"]
+  big_break_time = config_values["big_break_minutes"]
+  pomodors_before_big_break = config_values["pomodors_before_big_break"]
 
   # Starts pomodoro counter
   pomadur = Pomadur(work_time, break_time, big_break_time, pomodors_before_big_break)
